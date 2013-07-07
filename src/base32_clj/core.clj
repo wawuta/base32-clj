5d9c72f9e38efcc3140359c3a0c911c17f77b27f
= <=>
2013-07-07 10:33:47 +0800

    first commit

---
+++ src/base32_clj/core.clj
@@ -0,0 +1,8 @@
+(ns ^{:doc "Base32 Encoding/Deonding in Clojure."
+      :author "wang wu tao"}
+    base32-clj.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/base32_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns base32-clj.core-test
+  (:use clojure.test
+        base32-clj.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
