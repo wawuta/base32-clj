22b3068ec8b7a6e677f0fd0ea3d81cae24e614be
= <=>
2013-07-14 10:10:55 +0800

    encode v1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject base32-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Base32 Enoding/Deconding in Clojure"
+  :url "http://github.com/wawuta/base32-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
--- src/base32_clj/core.clj
+++ src/base32_clj/core.clj
@@ -12,23 +12,23 @@
 
 (defmacro write-base32
   [b o0 o1 o2 o3 o4]
-  (let [v (gensym "v")]
-    `(let [~v (long
+  (let (v (gensym "v"))
+   `(let [~v (long
                (bit-or
-                (<< (->int ~o0) 32)
-                ~(if o1 `(<< (->int ~o1) 24) 0)
-                ~(if o2 `(<< (->int ~o2) 16) 0)
-                ~(if o3 `(<< (->int ~o3) 8) 0)
-                ~(if o4 `(->int ~o4) 0)))]
-       (doto ~b
-         (.put (int->base32-byte (get-five ~v 0)))
-         (.put (int->base32-byte (get-five ~v 1)))
-         (.put ~(if o1 `(int->base32-byte (get-five ~v 2)) `BASE32_PAD))
-         (.put ~(if o1 `(int->base32-byte (get-five ~v 3)) 'BASE32_PAD))
-         (.put ~(if (and o1 o2) `(int->base32-byte (get-five ~v 4)) `BASE32_PAD))
-         (.put ~(if (and o1 o2 o3) `(int->base32-byte (get-five ~v 5)) `BASE32_PAD))
-         (.put ~(if (and o1 o2 o3) `(int->base32-byte (get-five ~v 6)) `BASE32_PAD))
-         (.put ~(if (and o1 o2 o3 o4) `(int->base32-byte (get-five ~v 7)) `BASE32_PAD))))))
+                 (<< (->int ~o0) 32)
+                 ~(if o1 `(<< (->int ~o1) 24) 0)
+                 ~(if o2 `(<< (->int ~o2) 16) 0)
+                 ~(if o3 `(<< (->int ~o3) 8) 0)
+                 ~(if o4 `(->int ~o4) 0)))]
+      (doto ~b
+        (.put (int->base32-byte (get-five ~v 0)))
+        (.put (int->base32-byte (get-five ~v 1)))
+        (.put ~(if o1 `(int->base32-byte (get-five ~v 2)) `BASE32_PAD))
+        (.put ~(if o1 `(int->base32-byte (get-five ~v 3)) 'BASE32_PAD))
+        (.put ~(if (and o1 o2) `(int->base32-byte (get-five ~v 4)) `BASE32_PAD))
+        (.put ~(if (and o1 o2 o3) `(int->base32-byte (get-five ~v 5)) `BASE32_PAD))
+        (.put ~(if (and o1 o2 o3) `(int->base32-byte (get-five ~v 6)) `BASE32_PAD))
+        (.put ~(if (and o1 o2 o3 o4) `(int->base32-byte (get-five ~v 7)) `BASE32_PAD))))))
 
 (defmacro write-octets
   [buffer data idx n]
@@ -38,7 +38,6 @@
      3 (write-base32 ~buffer (get-byte-at ~data ~idx 0) (get-byte-at ~data ~idx 1) (get-byte-at ~data ~idx 2) nil nil)
      4 (write-base32 ~buffer (get-byte-at ~data ~idx 0) (get-byte-at ~data ~idx 1) (get-byte-at ~data ~idx 2) (get-byte-at ~data ~idx 3) nil)
      5 (write-base32 ~buffer (get-byte-at ~data ~idx 0) (get-byte-at ~data ~idx 1) (get-byte-at ~data ~idx 2) (get-byte-at ~data ~idx 3) (get-byte-at ~data ~idx 4))))
-
 
 (defn encode-bytes
   ^"[B"
@@ -54,10 +53,33 @@
           (recur next-i))
         (.array b)))))
 
-
 (defn encode
   "Encode the given String to its UTF-8 Base32 Representataion."
   ([^String s] (encode s "UTF-8"))
   ([^String s ^String encoding]
      (let [data (encode-bytes (.getBytes s encoding))]
        (String. data "UTF-8"))))
+
+
+(defmacro read-base32
+  [b s0 s1 s2 s3 s4 s5 s6 s7]
+  `(let [v# (long (bit-or (<< ~s0 35) (<< ~s1 30) (<< (or ~s2 0) 25) (<< (or ~s3 0) 20) (<< (or ~s4 0) 15) (<< (or ~s5 0) 10) (<< (or ~s6 0) 5) (or ~s7 0)))]
+     (.put ~b (get-octet v# 0))
+     (when (and ~s2 ~s3)
+       (.put ~b (get-octet v# 1))
+       (when ~s4
+         (.put ~b (get-octet v# 2))
+         (when (and ~s5 ~s6)
+           (.put ~b (get-octet v# 3))
+           (when ~s7
+             (.put ~b (get-octet v# 4))))))))
+
+(defmacro read-five
+  [buffer data idx]
+  `(let [s0# (get-byte-at ~data ~idx 0)
+         s1# (get-byte-at ~data ~idx 1)
+         s2# (get-byte-at ~data ~idx 2)
+         s3# (get-byte-at ~data ~idx 3)
+         s4# (get-byte-at ~data ~idx 4)]
+     (read-base32 ~buffer
+         (base32-byte->int s0#))))
